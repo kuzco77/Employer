36a297b53a827a2fdc489c985fc4b183b6aa20e0
Chu Nam Anh <khuyendaxoa1996@yahoo.com.vn>
2016-06-16 23:30:24 +0700

    Initial Commit

---
+++ Employer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Employer
+//
+//  Created by Chu Nam Anh on 6/16/16.
+//  Copyright © 2016 Chu Nam Anh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
